010b278463f6cef2de47bf2697ec56905e1e57aa
Peter Ershow <ershowpeter@gmail.com>
2025-07-17 16:59:15 +0200

    invitation

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/', // Make sure this matches your deployment
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
